656a88de251c99ea439332089098fe083bc7a41f
Steve Gehrman <sgehrman@gmail.com>
2021-08-29 06:30:36 -0700

    fix

--- status_bar/example/lib/main.dart
+++ status_bar/example/lib/main.dart
@@ -24,6 +24,8 @@ class _MyAppState extends State<MyApp> {
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
+    StatusBar.showStatusBar();
+
     String platformVersion;
     // Platform messages may fail, so we use a try/catch PlatformException.
     // We also handle the message potentially returning null.
