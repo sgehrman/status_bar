c00ef4c5ee917ef72893cf04df11dc02aa680a6c
Steve Gehrman <sgehrman@gmail.com>
2021-08-29 16:56:08 -0700

    fix

--- status_bar_linux/linux/status_bar_plugin.cc
+++ status_bar_linux/linux/status_bar_plugin.cc
@@ -3,8 +3,9 @@
 #include <flutter_linux/flutter_linux.h>
 #include <gtk/gtk.h>
 #include <sys/utsname.h>
-
 #include <cstring>
+
+#include "tray.h"
 
 #define status_bar_PLUGIN(obj)                                     \
   (G_TYPE_CHECK_INSTANCE_CAST((obj), status_bar_plugin_get_type(), \
@@ -13,9 +14,59 @@
 struct _status_barPlugin
 {
   GObject parent_instance;
+
+  SystemTray *system_tray;
 };
 
 G_DEFINE_TYPE(status_barPlugin, status_bar_plugin, g_object_get_type())
+
+static FlMethodResponse *init_system_tray(status_barPlugin *self,
+                                          FlValue *args)
+{
+  g_autoptr(FlValue) result = fl_value_new_bool(FALSE);
+  FlMethodResponse *response = nullptr;
+
+  // if (fl_value_get_type(args) != FL_VALUE_TYPE_MAP)
+  // {
+  //   response = FL_METHOD_RESPONSE(
+  //       fl_method_error_response_new("Bad Arguments Error", "not map", nullptr));
+  //   break;
+  // }
+
+  const gchar *title = "nullptr";
+  const gchar *icon_path = "/home/steve/Pictures/coup.jpeg";
+  const gchar *tool_tip = "nullptr";
+
+  // FlValue *title_value = fl_value_lookup_string(args, kTitleKey);
+  // if (title_value && fl_value_get_type(title_value) == FL_VALUE_TYPE_STRING)
+  // {
+  //   title = fl_value_get_string(title_value);
+  // }
+
+  // FlValue *icon_path_value = fl_value_lookup_string(args, kIconPathKey);
+  // if (icon_path_value &&
+  //     fl_value_get_type(icon_path_value) == FL_VALUE_TYPE_STRING)
+  // {
+  //   icon_path = fl_value_get_string(icon_path_value);
+  // }
+
+  // FlValue *tooltip_value = fl_value_lookup_string(args, kToolTipKey);
+  // if (tooltip_value &&
+  //     fl_value_get_type(tooltip_value) == FL_VALUE_TYPE_STRING)
+  // {
+  //   tool_tip = fl_value_get_string(tooltip_value);
+  // }
+
+  result = fl_value_new_bool(
+      self->system_tray->init_system_tray(title, icon_path, tool_tip));
+
+  if (nullptr == response)
+  {
+    response = FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+  }
+
+  return response;
+}
 
 // Called when a method call is received from Flutter.
 static void status_bar_plugin_handle_method_call(
@@ -36,9 +87,9 @@ static void status_bar_plugin_handle_method_call(
   }
   else if (strcmp(method, "showStatusBar") == 0)
   {
-    g_autoptr(FlValue) result = fl_value_new_bool(true);
+    FlValue *args = fl_method_call_get_args(method_call);
 
-    response = FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+    response = init_system_tray(self, args);
   }
   else if (strcmp(method, "hideStatusBar") == 0)
   {
@@ -100,6 +151,8 @@ void status_bar_plugin_register_with_registrar(FlPluginRegistrar *registrar)
   fl_method_channel_set_method_call_handler(channel, method_call_cb,
                                             g_object_ref(plugin),
                                             g_object_unref);
+
+  plugin->system_tray = new SystemTray();
 
   g_object_unref(plugin);
 }
