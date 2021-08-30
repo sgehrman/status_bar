3ee03189309d31f2016308d1f5f2e58c69a55a14
Steve Gehrman <sgehrman@gmail.com>
2021-08-30 00:45:10 -0700

    fix

--- status_bar/example/lib/main.dart
+++ status_bar/example/lib/main.dart
@@ -2,10 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:status_bar/status_bar_plugin.dart';
-
-typedef SystemTrayEventCallback = void Function(String eventName);
-const String _kIconPathKey = "iconPath";
+import 'package:status_bar_example/status_bar_manager.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,8 +15,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
-  List<StatusMenuItem>? _menuItemCache;
-  StatusBarPlugin plugin = StatusBarPlugin();
+  final StatusBarManager statusBarManager = StatusBarManager();
 
   @override
   void initState() {
@@ -29,25 +25,10 @@ class _MyAppState extends State<MyApp> {
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
-    plugin.showStatusBar(<String, dynamic>{
-      _kIconPathKey: '/home/steve/Pictures/baby.jpg',
-    });
-
-    setContextMenu();
-
-    // call back for menu items
-    plugin.outputCallback = (methodCall) {
-      if (methodCall.method == 'status_bar_menu') {
-        final int menuItemId = methodCall.arguments;
-
-        _performMenuAction(_menuItems(), menuItemId);
-      }
-    };
-
     String platformVersion;
     try {
-      platformVersion =
-          await plugin.platformVersion ?? 'Unknown platform version';
+      platformVersion = await statusBarManager.plugin.platformVersion ??
+          'Unknown platform version';
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
@@ -57,98 +38,6 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       _platformVersion = platformVersion;
     });
-  }
-
-  void _performMenuAction(List<StatusMenuItem> items, int actionId) {
-    for (final item in items) {
-      if (item.id == actionId) {
-        item.onTap?.call();
-      } else {
-        if (item.children != null && item.children!.isNotEmpty) {
-          _performMenuAction(item.children!, actionId);
-        }
-      }
-    }
-  }
-
-  List<StatusMenuItem> _menuItems() {
-    if (_menuItemCache != null) {
-      return _menuItemCache!;
-    }
-    int menuItemId = 1;
-
-    _menuItemCache = [
-      StatusMenuItem(
-        type: StatusMenuItemType.item,
-        id: menuItemId++,
-        label: 'Show',
-        onTap: () {
-          // _appWindow.show();
-        },
-      ),
-      StatusMenuItem(
-        type: StatusMenuItemType.item,
-        id: menuItemId++,
-        label: 'Hide',
-        onTap: () {
-          // _appWindow.hide();
-        },
-      ),
-      StatusMenuItem.separator(),
-      StatusMenuItem(
-        id: menuItemId++,
-        type: StatusMenuItemType.submenu,
-        label: "SubMenu",
-        children: [
-          StatusMenuItem(
-            id: menuItemId++,
-            type: StatusMenuItemType.item,
-            label: 'SubItem1',
-            enabled: false,
-            onTap: () {
-              print("click SubItem1");
-            },
-          ),
-          StatusMenuItem(
-            id: menuItemId++,
-            type: StatusMenuItemType.item,
-            label: 'SubItem2',
-            onTap: () {
-              print("click SubItem2");
-            },
-          ),
-          StatusMenuItem(
-            id: menuItemId++,
-            type: StatusMenuItemType.item,
-            label: 'SubItem3',
-            onTap: () {
-              print("click SubItem3");
-            },
-          ),
-        ],
-      ),
-      StatusMenuItem.separator(),
-      StatusMenuItem(
-        id: menuItemId++,
-        type: StatusMenuItemType.item,
-        label: 'Quit',
-        onTap: () {
-          SystemNavigator.pop();
-        },
-      ),
-    ];
-
-    return _menuItemCache!;
-  }
-
-  Future<void> setContextMenu() async {
-    try {
-      final menus = _menuItems();
-
-      plugin.setStatusBarMenu(menus);
-    } on PlatformException catch (e) {
-      print('Platform exception setting menu: ${e.message}');
-    }
   }
 
   @override
---
+++ status_bar/example/lib/status_bar_manager.dart
@@ -0,0 +1,121 @@
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:status_bar/status_bar_plugin.dart';
+
+class StatusBarManager {
+  StatusBarManager() {
+    _setupStatusBar();
+  }
+
+  List<StatusMenuItem>? _menuItemCache;
+  StatusBarPlugin plugin = StatusBarPlugin();
+
+  void _setupStatusBar() {
+    plugin.showStatusBar(<String, dynamic>{
+      'iconPath': '/home/steve/Pictures/baby.jpg',
+    });
+
+    _setContextMenu();
+
+    // call back for menu items
+    plugin.outputCallback = (methodCall) {
+      if (methodCall.method == 'status_bar_menu') {
+        final int menuItemId = methodCall.arguments;
+
+        _performMenuAction(_menuItems(), menuItemId);
+      }
+    };
+  }
+
+  void _performMenuAction(List<StatusMenuItem> items, int actionId) {
+    for (final item in items) {
+      if (item.id == actionId) {
+        item.onTap?.call();
+      } else {
+        if (item.children != null && item.children!.isNotEmpty) {
+          _performMenuAction(item.children!, actionId);
+        }
+      }
+    }
+  }
+
+  List<StatusMenuItem> _menuItems() {
+    if (_menuItemCache != null) {
+      return _menuItemCache!;
+    }
+    int menuItemId = 1;
+
+    _menuItemCache = [
+      StatusMenuItem(
+        type: StatusMenuItemType.item,
+        id: menuItemId++,
+        label: 'Show',
+        onTap: () {
+          // _appWindow.show();
+        },
+      ),
+      StatusMenuItem(
+        type: StatusMenuItemType.item,
+        id: menuItemId++,
+        label: 'Hide',
+        onTap: () {
+          // _appWindow.hide();
+        },
+      ),
+      StatusMenuItem.separator(),
+      StatusMenuItem(
+        id: menuItemId++,
+        type: StatusMenuItemType.submenu,
+        label: "SubMenu",
+        children: [
+          StatusMenuItem(
+            id: menuItemId++,
+            type: StatusMenuItemType.item,
+            label: 'SubItem1',
+            enabled: false,
+            onTap: () {
+              print("click SubItem1");
+            },
+          ),
+          StatusMenuItem(
+            id: menuItemId++,
+            type: StatusMenuItemType.item,
+            label: 'SubItem2',
+            onTap: () {
+              print("click SubItem2");
+            },
+          ),
+          StatusMenuItem(
+            id: menuItemId++,
+            type: StatusMenuItemType.item,
+            label: 'SubItem3',
+            onTap: () {
+              print("click SubItem3");
+            },
+          ),
+        ],
+      ),
+      StatusMenuItem.separator(),
+      StatusMenuItem(
+        id: menuItemId++,
+        type: StatusMenuItemType.item,
+        label: 'Quit',
+        onTap: () {
+          SystemNavigator.pop();
+        },
+      ),
+    ];
+
+    return _menuItemCache!;
+  }
+
+  Future<void> _setContextMenu() async {
+    try {
+      final menus = _menuItems();
+
+      plugin.setStatusBarMenu(menus);
+    } on PlatformException catch (e) {
+      print('Platform exception setting menu: ${e.message}');
+    }
+  }
+}
