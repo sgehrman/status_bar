4690ce35e24063b9a8959e6ad0fee527b6333fec
Steve Gehrman <sgehrman@gmail.com>
2022-09-01 20:53:37 -0700

    fix

--- status_bar_platform_interface/lib/src/status_bar_platform.dart
+++ status_bar_platform_interface/lib/src/status_bar_platform.dart
@@ -1,4 +1,3 @@
-import 'dart:typed_data';
 import 'package:flutter/services.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 import 'method_channel_status_bar.dart';
